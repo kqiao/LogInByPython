aaf05629174f6e25ff75d3fad41b27929164e9ad
qiao ke <kqiao2011@126.com>
2013-05-28 20:20:55 +0800

    login by python

---
+++ logPostgraduateWebsite.py
@@ -0,0 +1,39 @@
+﻿
+import cookielib, urllib, urllib2
+import time, sys
+import argparse
+
+# 使用argparse添加命令行参数
+parser = argparse.ArgumentParser(description="Login into postgraduate website")
+parser.add_argument('username', help='The login username')
+parser.add_argument('password', help='The login password')
+args = parser.parse_args()
+
+# Step1. 添加cookie处理器
+'''注意：在追踪登录研究生信息系统会话的过程中，发现必须先登录entryUrl得到JSESSION值，
+   该值在登录loginUrl中需要作为请求头进行发送。另外具体登录那个url，应该以捕获到的会话信息为准，
+   而不是浏览器地址栏显示出的结果'''
+entryUrl = "http://210.27.12.1:90/student/index.jsp"
+loginUrl = "http://210.27.12.1:90/j_security_check"
+
+cl = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cl))
+urllib2.install_opener(opener)
+
+urllib2.urlopen(entryUrl)	#获取临时JSESSION值
+para = {'j_username' : args.username,
+        'j_password' : args.password,
+       }
+
+postData = urllib.urlencode(para)
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+req = urllib2.Request(url = loginUrl, data = postData, headers = headers)
+
+next = urllib2.urlopen(req)		#open this page
+print next.geturl()
+
+print "Now time: ",
+cur_time = time.localtime();
+print time.strftime("%a, %d %b %Y %H, %H:%M:%S ", cur_time)
+# time.sleep(10)	#sleep的参数单位为秒
---
+++ loginLibrary.py
@@ -0,0 +1,38 @@
+﻿import cookielib, urllib, urllib2, sys
+import argparse
+
+'''如何使用argparse，进行参数传递'''
+# Step1.创建一个ArgumentParser对象，可以添加description参数表示对该程序的说明
+parser = argparse.ArgumentParser(description="Login into XiDian Library")
+# Step2.使用parser.add_argument添加参数
+parser.add_argument("username", help="The login username")
+parser.add_argument("password", help="The login password")
+# Step3.解析参数，将存入的参数放入args中
+args = parser.parse_args()
+
+
+'''如何模拟登陆一个网站'''
+entryUrl = " http://202.117.122.8/patroninfo/ "
+# Step1.设置cookie处理器：1）得到cookieJar对象；2）HTTPCookieProcessor安装hanlder;3)安装打开器
+cl = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cl))
+urllib2.install_opener(opener)
+
+# Step2.使用urlopen打开URL之前，创建postData，使用urllib.urlencode()进行编码
+para = {'code' : args.username,
+        'pin' : args.password,
+       }
+postData = urllib.urlencode(para)
+
+# Step3.使用post方法打开url地址。返回一个文件类对象，但是具有两个额外操作geturl()和info()
+next = urllib2.urlopen(entryUrl, postData)
+
+print next.geturl()
+print next.info()
+print "----------------------"
+
+'''如何转换一个字符串的格式'''
+# Step1.获取当前系统的编码sys.getfilesystemcoding()
+type = sys.getfilesystemencoding()
+# Step2.对字符串先解编码decode，在用系统编码格式进行encode
+print next.read().decode("UTF_8").encode(type)
